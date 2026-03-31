13792ec7095caeb4970c76f3ef14a0814b9a1739
fabiokounang <fabiokounang11@gmail.com>
2026-03-31 09:57:06 +0700

    finish

--- utils/db.js
+++ utils/db.js
@@ -3,14 +3,18 @@ require('dotenv').config();
 
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT || 3306),
+    port: Number(process.env.DB_PORT || 4000),
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
-    decimalNumbers: true
+    decimalNumbers: true,
+    ssl: {
+        minVersion: 'TLSv1.2',
+        rejectUnauthorized: true
+    }
 });
 
 async function test_connection() {
